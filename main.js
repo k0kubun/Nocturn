89fb865cc83f04fcef8e7707f512bff7584a98e0
Takashi Kokubun <takashikkbn@gmail.com>
2015-08-23 02:33:02 +0900

    Use a simple directory structure

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-var app = require('app'); // Module to control application life.
-var BrowserWindow = require('browser-window'); // Module to create native browser window.
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
+var app = require('app');
+var BrowserWindow = require('browser-window');
 var mainWindow = null;
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
     app.quit();
@@ -14,11 +10,8 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/src/timeline/index.html');
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 });
