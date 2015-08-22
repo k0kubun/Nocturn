e26bc8e772c17867327f09cdf121f69dfb303f02
Takashi Kokubun <takashikkbn@gmail.com>
2015-08-23 02:30:21 +0900

    It was not necessary for app bootstrap
    Revert "Refactor main.js using ES6"
    This reverts commit 54936fcfb43e964514bea85154adbe1f0490051e.

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-require('electron-compile').init()
+var app = require('app'); // Module to control application life.
+var BrowserWindow = require('browser-window'); // Module to create native browser window.
 
-var Application = require('./src/browser/application');
-new Application();
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
