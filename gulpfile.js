cdc84464505417677aedd3b6d74a81f854941e7a
Takashi Kokubun <takashikkbn@gmail.com>
2015-08-26 23:20:16 +0900

    Compress css to load faster

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp   = require('gulp');
+var bower  = require('gulp-bower');
 var coffee = require('gulp-coffee');
 var haml   = require('gulp-haml');
-var sass   = require('gulp-sass');
+var sass   = require('gulp-ruby-sass');
+
+gulp.task(
+  'bower',
+  function() {
+    bower()
+      .pipe(gulp.dest('./bower_components'))
+  }
+)
 
 gulp.task(
   'compile',
@@ -10,7 +19,7 @@ gulp.task(
 
 gulp.task(
   'compile-coffee',
-  function () {
+  function() {
     gulp.src('src/**/*.coffee')
       .pipe(coffee())
       .pipe(gulp.dest('app'));
@@ -19,7 +28,7 @@ gulp.task(
 
 gulp.task(
   'compile-haml',
-  function () {
+  function() {
     gulp.src('static/**/*.haml')
       .pipe(haml())
       .pipe(gulp.dest('app'));
@@ -28,9 +37,12 @@ gulp.task(
 
 gulp.task(
   'compile-scss',
-  function () {
-    gulp.src('static/**/*.scss')
-      .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
-      .pipe(gulp.dest('app'));
+  function() {
+    sass('static/', {
+      style: 'compressed',
+      loadPath: [
+        './bower_components/bootstrap-sass/assets/stylesheets'
+      ]
+    }).pipe(gulp.dest('./app'))
   }
-)
+);
