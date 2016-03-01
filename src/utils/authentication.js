b9adee4da7498c29eddaf5033453478649fca8a2
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-02 01:41:18 +0900

    Refactor Authentication

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,60 +1,58 @@
-var BrowserWindow = require("browser-window");
-var JsonLoader = require("./json-loader");
-var NodeTwitterApi = require("node-twitter-api");
-var TwitterClient = require("./twitter-client");
-var authWindow = null;
+import { BrowserWindow } from 'electron'
+import JsonLoader        from './json-loader'
+import NodeTwitterApi    from 'node-twitter-api'
+import TwitterClient     from './twitter-client'
+
+const ACCOUNTS_JSON    = 'accounts.json';
+const CREDENTIALS_JSON = 'credentials.json';
+
+let authWindow = null;
 
 export default class Authentication {
-  static json_storage() {
-    return "accounts.json";
-  }
-
   static authorized(callback) {
     var token = Authentication.defaultAccount();
 
-    if (token && token["accessToken"]) {
+    if (token && token['accessToken']) {
       callback();
       return;
     }
 
-    return new Authentication(function(token) {
-      return Authentication.addToken(token, function() {
+    return new Authentication((token) => {
+      return Authentication.addToken(token, () => {
         return callback();
       });
     });
   }
 
   static addToken(token, callback) {
-    var tokens = JsonLoader.read(Authentication.json_storage());
+    var tokens = JsonLoader.read(ACCOUNTS_JSON);
     if (tokens == null) {
       tokens = [];
     }
     tokens.push(token);
-    JsonLoader.write(Authentication.json_storage(), Authentication.uniqTokens(tokens));
+    JsonLoader.write(ACCOUNTS_JSON, Authentication.uniqTokens(tokens));
     return callback();
   }
 
   static uniqTokens(tokens) {
-    var names = [];
+    var names  = [];
     var uniqed = [];
 
     for (let token of tokens) {
-      if (names.indexOf(token["screenName"]) < 0) {
+      if (names.indexOf(token['screenName']) < 0) {
         uniqed.push(token);
-        names.push(token["screenName"]);
+        names.push(token['screenName']);
       }
     }
-
     return uniqed;
   }
 
   static byScreenName(screenName) {
-    for (let account in Authentication.allAccounts()) {
-      if (account["screenName"] === screenName) {
+    for (let account of Authentication.allAccounts()) {
+      if (account['screenName'] === screenName) {
         return account;
       }
     }
-
     return {};
   }
 
@@ -64,21 +62,20 @@ export default class Authentication {
     if (accounts.length === 0) {
       return {};
     }
-
     return accounts[0];
   }
 
   static allAccounts() {
-    return JsonLoader.read(Authentication.json_storage()) || [];
+    return JsonLoader.read(ACCOUNTS_JSON) || [];
   }
 
   constructor(callback) {
-    var credentials = JsonLoader.read("credentials.json");
+    var credentials = JsonLoader.read(CREDENTIALS_JSON);
 
     var nodeTwitterApi = new NodeTwitterApi({
-      callback: "http://example.com",
-      consumerKey: credentials["consumerKey"],
-      consumerSecret: credentials["consumerSecret"]
+      callback:       'http://example.com',
+      consumerKey:    credentials['consumerKey'],
+      consumerSecret: credentials['consumerSecret'],
     });
 
     var klass = this;
@@ -86,58 +83,58 @@ export default class Authentication {
     nodeTwitterApi.getRequestToken((error, requestToken, requestTokenSecret) => {
       var oldWindow;
       var url = nodeTwitterApi.getAuthUrl(requestToken);
-      (authWindow ? oldWindow = authWindow : undefined);
+      if (authWindow) {
+        oldWindow = authWindow;
+      }
 
       authWindow = new BrowserWindow({
-        width: 800,
+        width:  800,
         height: 600,
-        "node-integration": false
+        'node-integration': false,
       });
 
-      authWindow.webContents.on("will-navigate", (event, url) => {
-        var matched;
-
-        if ((matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/))) {
+      authWindow.webContents.on('will-navigate', (event, url) => {
+        var matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/);
+        if (matched) {
           event.preventDefault();
 
-          return nodeTwitterApi.getAccessToken(
-            requestToken,
-            requestTokenSecret,
-            matched[2],
-            (error, accessToken, accessTokenSecret) => {
-              if (error) {
-                return new Authentication(callback);
-              } else {
-                var token = {
-                  accessToken: accessToken,
-                  accessTokenSecret: accessTokenSecret
-                };
+          return nodeTwitterApi.getAccessToken(requestToken, requestTokenSecret, matched[2], (error, accessToken, accessTokenSecret) => {
+            if (error) {
+              return new Authentication(callback);
+            } else {
+              var token = { accessToken: accessToken, accessTokenSecret: accessTokenSecret };
+              var client = new TwitterClient(token);
 
-                var client = new TwitterClient(token);
-
-                return client.verifyCredentials(function(user) {
-                  token["screenName"] = user.screen_name;
-                  token["profileImage"] = user.profile_image_url;
-                  callback(token);
-                  return (authWindow ? authWindow.close() : undefined);
-                });
-              }
+              return client.verifyCredentials((user) => {
+                token["screenName"]   = user.screen_name;
+                token["profileImage"] = user.profile_image_url;
+                callback(token);
+                if (authWindow) {
+                  authWindow.close();
+                }
+              });
             }
-          );
-        } else if (url.match(/\/account\/login_verification/))
-          {} else if (url.match(/\/oauth\/authenticate/))
-          {} else {
+          });
+        } else if (url.match(/\/account\/login_verification/)) {
+          // noop (start of 2FA session)
+        } else if (url.match(/\/oauth\/authenticate/)) {
+          // noop (redirection to successful callback)
+        } else {
           event.preventDefault();
+
+          // regard current session as invalid submission and retry
           return new Authentication(callback);
         }
       });
 
-      authWindow.on("closed", function() {
+      authWindow.on('closed', function() {
         return authWindow = null;
       });
 
-      (oldWindow ? oldWindow.close() : undefined);
-      return authWindow.loadURL(url + "&force_login=true");
+      if (oldWindow) {
+        oldWindow.close();
+      }
+      return authWindow.loadURL(`${url}&force_login=true`);
     });
   }
-};
+}
