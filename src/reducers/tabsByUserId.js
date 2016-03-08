40ffa2e7306d60efd7ad4270e0474fcc6daf43f8
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-09 01:15:36 +0900

    Implement sorted set by myself

--- src/reducers/tabsByUserId.js
+++ src/reducers/tabsByUserId.js
@@ -1,5 +1,19 @@
 import Actions from '../actions';
-import SortedSet from 'js-sorted-set';
+
+// Return set order by id DESC.
+const sortedAdd = (set, tweet) => {
+  for (let i in set) {
+    let result = set[i].id_str.localeCompare(tweet.id_str);
+    if (result == -1) {
+      // If set[i].id_str is smaller than tweet.id_str, prepend tweet to set[i].
+      return [...set.slice(0, i), tweet, ...set.slice(i)];
+    } else if (result == 0) {
+      // If tweet is already exist, ignore it.
+      return set;
+    }
+  }
+  return [tweet, ...set];
+};
 
 export const tabsByUserId = (state = {}, action) => {
   switch (action.type) {
@@ -8,11 +22,9 @@ export const tabsByUserId = (state = {}, action) => {
         ...state,
         [action.account.id]: {
           ...(state[action.account.id] || {}),
-          [action.tab]: (
-            (
-              (state[action.account.id] && state[action.account.id][action.tab]) ||
-              new SortedSet({ comparator: (a, b) => { return b.id_str.localeCompare(a.id_str) } })
-            ).insert(action.tweet)
+          [action.tab]: sortedAdd(
+            (state[action.account.id] && state[action.account.id][action.tab]) || [],
+            action.tweet
           ),
         }
       };
