5b647e9a744190febb43cf1f3ea2dc9e92763057
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-09 23:26:31 +0900

    Correctly detect user info update

--- src/reducers/userByUserId.js
+++ src/reducers/userByUserId.js
@@ -3,8 +3,10 @@ import Actions from '../actions';
 export const userByUserId = (state = {}, action) => {
   switch (action.type) {
     case Actions.REFRESH_USER_INFO:
-      state[action.user.id] = action.user;
-      return state;
+      return {
+        ...state,
+        [action.user.id]: action.user,
+      };
     default:
       return state;
   }
