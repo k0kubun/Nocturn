ce9fe733b1c3a814318d92e2907b81aaa67e386d
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-11 22:22:31 +0900

    Initialize linux menu

--- src/utils/MenuBuilder.js
+++ src/utils/MenuBuilder.js
@@ -71,26 +71,29 @@ export default class MenuBuilder {
   static templateForOthers(mainWindow) {
     return [
       {
-        label: '&File', submenu: [
-          { label: '&Open', accelerator: 'Ctrl+O' },
-          { label: '&Close', accelerator: 'Ctrl+W', click() { mainWindow.close(); } },
+        label: 'Nocturn',
+        submenu: [
+          { label: 'Quit', accelerator: 'Ctrl+W', click() { mainWindow.close(); } },
         ],
       }, {
-        label: '&View',
+        label: 'View',
         submenu: (process.env.NODE_ENV === 'development') ? [
           { label: '&Reload', accelerator: 'Ctrl+R', click() { mainWindow.reload(); } },
-          { label: 'Toggle &Full Screen', accelerator: 'F11', click() { mainWindow.setFullScreen(!mainWindow.isFullScreen()); } },
-          { label: 'Toggle &Developer Tools', accelerator: 'Alt+Ctrl+I', click() { mainWindow.toggleDevTools(); } },
+          { label: 'Toggle Full Screen', accelerator: 'F11', click() { mainWindow.setFullScreen(!mainWindow.isFullScreen()); } },
+          { label: 'Toggle Developer Tools', accelerator: 'Alt+Ctrl+I', click() { mainWindow.toggleDevTools(); } },
         ] : [
-          { label: 'Toggle &Full Screen', accelerator: 'F11', click() { mainWindow.setFullScreen(!mainWindow.isFullScreen()); } },
+          { label: 'Toggle Full Screen', accelerator: 'F11', click() { mainWindow.setFullScreen(!mainWindow.isFullScreen()); } },
+        ]
+      }, {
+        label: 'Tweet',
+        submenu: [
+          { label: 'Reply', accelerator: 'Enter', click() { console.log('stub'); } },
         ]
       }, {
         label: 'Help',
         submenu: [
-          { label: 'Learn More', click() { shell.openExternal('http://electron.atom.io'); } },
-          { label: 'Documentation', click() { shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme'); } },
-          { label: 'Community Discussions', click() { shell.openExternal('https://discuss.atom.io/c/electron'); } },
-          { label: 'Search Issues', click() { shell.openExternal('https://github.com/atom/electron/issues'); } },
+          { label: 'Nocturn Homepage', click() { shell.openExternal('http://github.com/k0kubun/Nocturn'); } },
+          { label: 'Search Issues', click() { shell.openExternal('https://github.com/k0kubun/Nocturn/issues'); } },
         ],
       },
     ];
