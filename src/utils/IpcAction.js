db304b15e621a38a4e3f4a5e1c38343034d04ffc
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-11 23:44:20 +0900

    Carve out rich state

--- src/utils/IpcAction.js
+++ src/utils/IpcAction.js
@@ -1,28 +1,43 @@
 import { ipcRenderer } from 'electron';
 import Actions         from '../actions';
 
-export default class IpcAction {
-  constructor(document) {
-    this.document = document;
+class RichState {
+  constructor(store) {
+    this.state = store.getState();
   }
 
-  subscribe(store) {
-    ipcRenderer.on('invoke-reply', (event) => {
-      let state = store.getState();
-      let activeAccount = state.accounts[state.activeAccountIndex];
-      let activeTab = state.selectedTabByUserId[activeAccount.id] || 'home';
-      let tweets = state.tabsByUserId[activeAccount.id] && state.tabsByUserId[activeAccount.id][activeTab];
-      let selectedTweetId = state.selectedTweetIdsByUserId[activeAccount.id] && state.selectedTweetIdsByUserId[activeAccount.id][activeTab];
+  activeAccount() {
+    if (this.activeAccountCache) return this.activeAccountCache;
+    return this.activeAccountCache = this.state.accounts[this.state.activeAccountIndex];
+  }
 
-      let tweet = this.findTweet(tweets, selectedTweetId);
-      if (!tweet) return null;
+  activeTab() {
+    if (this.activeTabCache) return this.activeTabCache;
+    return this.activeTabCache = (this.state.selectedTabByUserId[this.activeAccount().id] || 'home');
+  }
 
-      store.dispatch(Actions.setText(`@${tweet.user.screen_name} `));
-      store.dispatch(Actions.setInReplyTo(tweet));
+  selectedTweetId() {
+    if (this.selectedTweetIdCache) return this.selectedTweetIdCache;
+    return this.selectedTweetIdCache = (
+      this.state.selectedTweetIdsByUserId[this.activeAccount().id] &&
+      this.state.selectedTweetIdsByUserId[this.activeAccount().id][this.activeTab()]
+    );
+  }
 
-      // FIXME: Use better way to focus
-      document.getElementById('tweet_editor').focus();
-    })
+  activeTabTweets() {
+    if (this.activeTabTweetsCache) return this.activeTabTweetsCache;
+    return this.activeTabTweetsCache = (
+      this.state.tabsByUserId[this.activeAccount().id] &&
+      this.state.tabsByUserId[this.activeAccount().id][this.activeTab()]
+    );
+  }
+
+  activeTweet() {
+    if (this.activeTweetCache) return this.activeTweetCache;
+    return this.activeTweetCache = this.findTweet(
+      this.activeTabTweets(),
+      this.selectedTweetId(),
+    );
   }
 
   findTweet(tweets, id_str) {
@@ -34,5 +49,25 @@ export default class IpcAction {
       }
     }
     return null;
+  }
+}
+
+export default class IpcAction {
+  constructor(document) {
+    this.document = document;
+  }
+
+  subscribe(store) {
+    ipcRenderer.on('invoke-reply', (event) => {
+      let state = new RichState(store);
+      let tweet = state.activeTweet();
+      if (!tweet) return null;
+
+      store.dispatch(Actions.setText(`@${tweet.user.screen_name} `));
+      store.dispatch(Actions.setInReplyTo(tweet));
+
+      // FIXME: Use better way to focus
+      this.document.getElementById('tweet_editor').focus();
+    })
   }
 }
