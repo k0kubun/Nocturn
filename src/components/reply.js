55f182cb0182a408c88f22145c147d494eba13de
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-13 16:18:23 +0900

    Delegate responsibility to ReplyContainer

--- src/components/reply.js
+++ src/components/reply.js
@@ -2,21 +2,12 @@ import React, { PropTypes } from 'react';
 
 export default class Reply extends React.Component {
   static propTypes = {
-    setInReplyTo: PropTypes.func.isRequired,
-    setText:      PropTypes.func.isRequired,
-  }
-
-  onReplyClicked(event) {
-    this.props.setText(`@${this.props.tweet.user.screen_name} `);
-    this.props.setInReplyTo(this.props.tweet);
-
-    // FIXME: Use better way to focus
-    event.target.ownerDocument.getElementById('tweet_editor').focus();
+    onClick: PropTypes.func.isRequired,
   }
 
   render() {
     return(
-      <div className='reply_widget_wrapper' onClick={this.onReplyClicked.bind(this)}>
+      <div className='reply_widget_wrapper' onClick={this.props.onClick}>
         <i className='fa fa-reply reply_button' />
       </div>
     );
--- src/containers/reply-container.js
+++ src/containers/reply-container.js
@@ -6,7 +6,16 @@ const mapStateToProps = (state) => {
   return {}
 }
 
-export default connect(mapStateToProps, {
-  ...Actions.texts,
-  ...Actions.tweets,
-})(Reply);
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    onClick: (event) => {
+      dispatch(Actions.texts.setText(`@${props.tweet.user.screen_name} `));
+      dispatch(Actions.tweets.setInReplyTo(props.tweet));
+
+      // FIXME: Use better way to focus
+      event.target.ownerDocument.getElementById('tweet_editor').focus();
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Reply);
