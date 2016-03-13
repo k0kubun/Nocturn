da6672c02d57ea3d227ea1c02105897967143658
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-13 16:21:52 +0900

    Delegate responsibility to FavoriteContainer

--- src/components/favorite.js
+++ src/components/favorite.js
@@ -1,24 +1,16 @@
 import React, { PropTypes } from 'react';
-import TwitterClient        from '../utils/twitter-client'
 
 export default class Favorite extends React.Component {
   static propTypes = {
-    account:  PropTypes.object.isRequired,
-    tweet:    PropTypes.object.isRequired,
-    tab:      PropTypes.string.isRequired,
-    addTweet: PropTypes.func.isRequired,
-  }
-
-  onFavoriteClicked(event) {
-    let client = new TwitterClient(this.props.account);
-    client.favoriteStatus(this.props.tweet.id_str, (tweet) => {
-      this.props.addTweet(tweet, this.props.account, this.props.tab);
-    });
+    account: PropTypes.object.isRequired,
+    tweet:   PropTypes.object.isRequired,
+    tab:     PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
   }
 
   render() {
     return(
-      <div className='favorite_widget_wrapper' onClick={this.onFavoriteClicked.bind(this)}>
+      <div className='favorite_widget_wrapper' onClick={this.props.onClick}>
         <i className={`fa fa-star favorite_button ${this.props.tweet.favorited ? 'active' : ''}`} />
       </div>
     );
--- src/containers/favorite-container.js
+++ src/containers/favorite-container.js
@@ -1,11 +1,21 @@
-import Actions     from '../actions';
-import Favorite    from '../components/favorite';
-import { connect } from 'react-redux';
+import Actions       from '../actions';
+import Favorite      from '../components/favorite';
+import TwitterClient from '../utils/twitter-client';
+import { connect }   from 'react-redux';
 
 const mapStateToProps = (state) => {
   return {}
 }
 
-export default connect(mapStateToProps, {
-  ...Actions.tweets,
-})(Favorite);
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    onClick: (event) => {
+      let client = new TwitterClient(props.account);
+      client.favoriteStatus(props.tweet.id_str, (tweet) => {
+        dispatch(Actions.tweets.addTweet(tweet, props.account, props.tab));
+      });
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
