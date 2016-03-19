9650ffbb77ae7b29101f0ac42f968769f634d62a
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-20 01:45:15 +0900

    Delegate responsibility of Account

--- src/components/account.js
+++ src/components/account.js
@@ -6,33 +6,17 @@ export default class Account extends React.Component {
   static propTypes = {
     activeAccount:   PropTypes.object,
     accounts:        PropTypes.array.isRequired,
-    addAccount:      PropTypes.func.isRequired,
-    refreshUserInfo: PropTypes.func.isRequired,
+    onAccountChange: PropTypes.func.isRequired,
+    subscribeIpc:    PropTypes.func.isRequired,
   }
 
   componentDidMount() {
-    ipcRenderer.on('finish-authentication', (event, token) => {
-      this.props.addAccount(token);
-
-      let client = new TwitterClient(token);
-      client.verifyCredentials((user) => {
-        this.props.refreshUserInfo(user);
-      });
-    });
-  }
-
-  onAccountChange(event) {
-    if (event.target.value === 'add-account') {
-      event.preventDefault();
-      ipcRenderer.send('start-authentication');
-    } else {
-      this.props.activateAccount(event.target.value);
-    }
+    this.props.subscribeIpc();
   }
 
   render() {
     return(
-      <select id='account_selector' name='account_list' onChange={this.onAccountChange.bind(this)} value={this.props.activeAccountIndex}>
+      <select id='account_selector' name='account_list' onChange={this.props.onAccountChange} value={this.props.activeAccountIndex}>
         {this.props.accounts.map((account, index) =>
           <option value={index} key={account.id_str}>{account.screenName}</option>
         )}
--- src/containers/account-container.js
+++ src/containers/account-container.js
@@ -1,6 +1,8 @@
-import { connect } from 'react-redux';
-import Actions     from '../actions';
-import Account     from '../components/account'
+import Account         from '../components/account';
+import Actions         from '../actions';
+import TwitterClient   from '../utils/twitter-client';
+import { connect }     from 'react-redux';
+import { ipcRenderer } from 'electron';
 
 const mapStateToProps = (state) => {
   return {
@@ -9,6 +11,27 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, {
-  ...Actions.accounts,
-})(Account);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAccountChange: (event) => {
+      if (event.target.value === 'add-account') {
+        event.preventDefault();
+        ipcRenderer.send('start-authentication');
+      } else {
+        dispatch(Actions.accounts.activateAccount(event.target.value));
+      }
+    },
+    subscribeIpc: () => {
+      ipcRenderer.on('finish-authentication', (event, token) => {
+        dispatch(Actions.accounts.addAccount(token));
+
+        let client = new TwitterClient(token);
+        client.verifyCredentials((user) => {
+          dispatch(Actions.accounts.refreshUserInfo(user));
+        });
+      });
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Account);
