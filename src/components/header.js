07ebc071573e975873afa7ee751ec5a36389a245
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-20 03:13:14 +0900

    Delegate responsibility of Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,65 +1,25 @@
 import React, { PropTypes } from 'react';
-import TimelineProxy        from '../utils/timeline-proxy'
-import TwitterClient        from '../utils/twitter-client'
-import { shell }            from 'electron';
 
 export default class Header extends React.Component {
   static propTypes = {
     activeAccount:     PropTypes.object,
     activeListId:      PropTypes.string,
     activeSearchQuery: PropTypes.string,
-    addTweet:          PropTypes.func.isRequired,
-  }
-
-  onHomeClicked() {
-    let url = `https://twitter.com/${this.props.activeAccount.screenName}`;
-    shell.openExternal(url);
-  }
-
-  onRefreshClicked() {
-    let client = new TwitterClient(this.props.activeAccount);
-    let proxy  = new TimelineProxy(this.props.addTweet, this.props.activeAccount);
-
-    client.homeTimeline((tweets) => {
-      for (let tweet of tweets) {
-        proxy.addTweet(tweet);
-      }
-    });
-    this.refreshListsTab(client);
-    this.refreshSearchTab(client);
-  }
-
-  refreshListsTab(client) {
-    if (this.props.activeListId) {
-      client.listsStatuses(this.props.activeListId, (tweets) => {
-        for (let tweet of tweets) {
-          this.props.addTweet(tweet, this.props.activeAccount, 'lists');
-        }
-      })
-    }
-  }
-
-  refreshSearchTab(client) {
-    if (this.props.activeSearchQuery) {
-      client.searchTweets(this.props.activeSearchQuery, (tweets) => {
-        for (let tweet of tweets) {
-          this.props.addTweet(tweet, this.props.activeAccount, 'search');
-        }
-      })
-    }
+    onHomeClicked:     PropTypes.func.isRequired,
+    onRefreshClicked:  PropTypes.func.isRequired,
   }
 
   render() {
     return(
       <div className='header clearfix'>
         <div className='button_wrapper'>
-          <div className='button home_button' onClick={this.onHomeClicked.bind(this)}>
+          <div className='button home_button' onClick={this.props.onHomeClicked}>
             <i className='fa fa-home'></i>
           </div>
           Home
         </div>
         <div className='button_wrapper'>
-          <div className='button refresh_button' onClick={this.onRefreshClicked.bind(this)}>
+          <div className='button refresh_button' onClick={this.props.onRefreshClicked}>
             <i className='fa fa-refresh'></i>
           </div>
           Refresh
--- src/containers/header-container.js
+++ src/containers/header-container.js
@@ -1,6 +1,9 @@
-import { connect } from 'react-redux';
-import Actions     from '../actions';
-import Header      from '../components/header'
+import Actions       from '../actions';
+import Header        from '../components/header';
+import TimelineProxy from '../utils/timeline-proxy';
+import TwitterClient from '../utils/twitter-client';
+import { connect }   from 'react-redux';
+import { shell }     from 'electron';
 
 const mapStateToProps = (state) => {
   let activeAccount = state.accounts[state.activeAccountIndex];
@@ -11,6 +14,53 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, {
-  ...Actions.tweets,
-})(Header);
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+  const { dispatch } = dispatchProps;
+
+  const addTweet = (tweet, account, tab) => {
+    dispatch(Actions.tweets.addTweet(tweet, account, tab));
+  }
+
+  const refreshListsTab = (client) => {
+    if (stateProps.activeListId) {
+      client.listsStatuses(stateProps.activeListId, (tweets) => {
+        for (let tweet of tweets) {
+          addTweet(tweet, stateProps.activeAccount, 'lists');
+        }
+      })
+    }
+  }
+
+  const refreshSearchTab = (client) => {
+    if (stateProps.activeSearchQuery) {
+      client.searchTweets(stateProps.activeSearchQuery, (tweets) => {
+        for (let tweet of tweets) {
+          addTweet(tweet, stateProps.activeAccount, 'search');
+        }
+      })
+    }
+  }
+
+  return {
+    ...ownProps,
+    ...stateProps,
+    onHomeClicked: () => {
+      let url = `https://twitter.com/${stateProps.activeAccount.screenName}`;
+      shell.openExternal(url);
+    },
+    onRefreshClicked: () => {
+      let client = new TwitterClient(stateProps.activeAccount);
+      let proxy  = new TimelineProxy(addTweet, stateProps.activeAccount);
+
+      client.homeTimeline((tweets) => {
+        for (let tweet of tweets) {
+          proxy.addTweet(tweet);
+        }
+      });
+      refreshListsTab(client);
+      refreshSearchTab(client);
+    },
+  }
+}
+
+export default connect(mapStateToProps, null, mergeProps)(Header);
