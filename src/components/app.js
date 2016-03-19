c0d379835dbf7ef12d35112616bfb34b270e2e96
Takashi Kokubun <takashikkbn@gmail.com>
2016-03-20 01:51:09 +0900

    Delegate responsibility of App

--- src/components/app.js
+++ src/components/app.js
@@ -1,32 +1,17 @@
 import React, { PropTypes } from 'react';
 import AccountContainer     from '../containers/account-container';
-import Authentication       from '../utils/authentication';
 import EditorContainer      from '../containers/editor-container';
 import HeaderContainer      from '../containers/header-container';
 import TimelineContainer    from '../containers/timeline-container';
-import TwitterClient        from '../utils/twitter-client';
 
 export default class App extends React.Component {
   static propTypes = {
-    accounts:        PropTypes.array.isRequired,
-    addAccount:      PropTypes.func.isRequired,
-    refreshUserInfo: PropTypes.func.isRequired,
+    accounts:           PropTypes.array.isRequired,
+    initializeAccounts: PropTypes.func.isRequired,
   }
 
   componentDidMount() {
-    this.initializeAccounts();
-  }
-
-  initializeAccounts() {
-    let accounts = Authentication.allAccounts();
-    for (let account of accounts) {
-      this.props.addAccount(account);
-
-      let client = new TwitterClient(account);
-      client.verifyCredentials((user) => {
-        this.props.refreshUserInfo(user);
-      });
-    }
+    this.props.initializeAccounts();
   }
 
   render() {
--- src/containers/app-container.js
+++ src/containers/app-container.js
@@ -1,6 +1,8 @@
-import { connect } from 'react-redux';
-import Actions     from '../actions';
-import App         from '../components/app'
+import { connect }    from 'react-redux';
+import Actions        from '../actions';
+import App            from '../components/app';
+import Authentication from '../utils/authentication';
+import TwitterClient  from '../utils/twitter-client';
 
 const mapStateToProps = (state) => {
   return {
@@ -8,6 +10,20 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, {
-  ...Actions.accounts,
-})(App);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    initializeAccounts: () => {
+      let accounts = Authentication.allAccounts();
+      for (let account of accounts) {
+        dispatch(Actions.accounts.addAccount(account));
+
+        let client = new TwitterClient(account);
+        client.verifyCredentials((user) => {
+          dispatch(Actions.accounts.refreshUserInfo(user));
+        });
+      }
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
