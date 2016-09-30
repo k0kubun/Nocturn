d024b4238032da7b049222d05f3ef30bf7b75081
IzumiSy <beetle-noise@gmx.com>
2016-10-01 02:01:47 +0900

    Now only user_name has a link to the user profile page

--- src/components/tweet-header.js
+++ src/components/tweet-header.js
@@ -13,29 +13,20 @@ export default class TweetHeader extends React.Component {
 
   render() {
     const tweetUser = this.props.tweet.user
-    const twitterExtLinkClasses = 'external-link external-link-twitter'
-    const classes = {
-      userName: `user_name ${twitterExtLinkClasses}`,
-      screenName: `screen_name ${twitterExtLinkClasses}`
-    }
 
     return (
       <div className='tweet_header clearfix'>
         <div className='name_wrapper'>
           <a
             href={this.generateTwitterProfileUrl(tweetUser.screen_name)}
-            className={classes.userName}
+            className='user_name external-link external-link-twitter'
             target='_blank'
           >
             {tweetUser.name}
           </a>
-          <a
-            href={this.generateTwitterProfileUrl(tweetUser.screen_name)}
-            className={classes.screenName}
-            target='_blank'
-          >
+          <span className='screen_name'>
             {tweetUser.screen_name}
-          </a>
+          </span>
         </div>
         <Time className='created_at' time={this.props.tweet.created_at} now={this.props.now} />
       </div>
