b9cd2036ef814c5d103b4104bdcc91474ff795ee
IzumiSy <beetle-noise@gmx.com>
2016-10-01 01:44:48 +0900

    Implement twitter profile url link generator

--- src/components/tweet-header.js
+++ src/components/tweet-header.js
@@ -7,12 +7,35 @@ export default class TweetHeader extends React.Component {
     now:   PropTypes.number,
   }
 
+  generateTwitterProfileUrl(screen_name) {
+    return `https://twitter.com/${screen_name}`
+  }
+
   render() {
+    const tweetUser = this.props.tweet.user
+    const nameWrapperCommonClasses = 'external-link external-link-twitter'
+    const classes = {
+      userName: `user_name ${nameWrapperCommonClasses}`,
+      screenName: `screen_name ${nameWrapperCommonClasses}`
+    }
+
     return (
       <div className='tweet_header clearfix'>
         <div className='name_wrapper'>
-          <span className='user_name'>{this.props.tweet.user.name}</span>
-          <span className='screen_name'>{this.props.tweet.user.screen_name}</span>
+          <a
+            href={this.generateTwitterProfileUrl(tweetUser.screen_name)}
+            className={classes.userName}
+            target='_blank'
+          >
+            {tweetUser.name}
+          </a>
+          <a
+            href={this.generateTwitterProfileUrl(tweetUser.screen_name)}
+            className={classes.screenName}
+            target='_blank'
+          >
+            {tweetUser.screen_name}
+          </a>
         </div>
         <Time className='created_at' time={this.props.tweet.created_at} now={this.props.now} />
       </div>
