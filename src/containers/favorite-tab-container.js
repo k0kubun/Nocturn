bdc68507d70cb9e9c0b511464866fb1196347eb4
IzumiSy <beetle-noise@gmx.com>
2016-10-04 13:37:23 +0900

    Add active prop

--- src/containers/favorite-tab-container.js
+++ src/containers/favorite-tab-container.js
@@ -3,17 +3,21 @@ import TweetTab    from '../components/tweet-tab';
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state, props) => {
+  const selectedTab = state.selectedTabByUserId[props.account.id_str] || 'home';
+
   return {
+    active: selectedTab === props.tab,
     unread: 0
   };
 }
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => {
-  const { unread } = stateProps;
+  const { active, unread } = stateProps;
   const { dispatch } = dispatchProps;
 
   return {
     ...ownProps,
+    active,
     unread,
     onTabClicked() {
       // TODO call dispatch and execute data fetching
