0aae486ea8bb35dce0c400f5d9f3ca0292a22c7d
Oliver Larsen <CandyFace@users.noreply.github.com>
2016-10-26 13:13:34 +0200

    Added hls support for media window (#50)
    * Added hls support for media window, dependency for hls.js added to package
    
    * Cosmetic changes
    
    FIXME: use import has been refactored for ES6, changed name of
    video.title for convenience and other small cosmetic changes
    
    * use once instead of on to avoid memory leaking listeners
    
    There is no need for us to set up a new listener for each time we open
    a window.
    FIXME: refactored ipcRenderer to ES6, but was that all?
    
    * More cosmetic changes, made it possible to drag window
    
    Also between this commit and the last, which involved using ‘once’
    instead of ‘on’ fixed the issue that previous windows would be resized
    to the new window size.
    
    * Padding should not be added to other platforms, height should always add 20
    
    * Removed spaces, aded semicolon to various lines.
    
    And other adjustments.

--- src/media-popup.js
+++ src/media-popup.js
@@ -1,21 +1,33 @@
 // FIXME: Refactor later
-var ipcRenderer = require('electron').ipcRenderer;
+import { ipcRenderer } from 'electron';
+
+var hls = require('hls.js');
 var img = document.getElementById('loadedImage');
 var video = document.getElementById('loadedVideo');
+var body = document.getElementsByTagName("body")[0];
+var hlsMedia = new hls();
 var mediaWidth = 0,
 mediaHeight = 0,
 maxWidth = 800,
 maxHeight = 600;
+
+if (process.platform !== 'darwin') {
+  body.style = "padding-top: 0px;";
+}
 
 function resizeMedia(media) {
   if (media.path[0].id === img.id) {
     mediaWidth = this.width;
     mediaHeight = this.height;
   } else {
-    mediaWidth = this.videoWidth;
-    mediaHeight = this.videoHeight;
+    if (video.title === "HLS media") {
+      mediaWidth = this.videoWidth * 4;
+      mediaHeight = this.videoHeight * 4;
+    } else {
+      mediaWidth = this.videoWidth;
+      mediaHeight = this.videoHeight;
+    }
   }
-
   // Scale media proportionally if larger than maxWidth and maxHeight
   if (mediaWidth > maxWidth) {
     mediaHeight = mediaHeight * (maxWidth / mediaWidth);
@@ -42,10 +54,15 @@ function resizeMedia(media) {
   ipcRenderer.send('imageDimensions', Math.round(mediaWidth), Math.round(mediaHeight));
 }
 
-ipcRenderer.on('load-media', (event, mediaUrl, mediaType) => {
-  if (mediaType !== "video/mp4"){
+ipcRenderer.once('load-media', (event, mediaUrl, mediaType) => {
+  if (mediaType !== "video/mp4" && mediaType !== "application/x-mpegURL"){
     img.src = mediaUrl;
-    img.onload = resizeMedia.bind(img)
+    img.onload = resizeMedia.bind(img);
+  } else if (mediaType === "application/x-mpegURL") {
+    hlsMedia.loadSource(mediaUrl);
+    hlsMedia.attachMedia(video);
+    video.onloadedmetadata = resizeMedia.bind(hlsMedia.media);
+    video.title = "HLS media";
   } else {
     video.src = mediaUrl;
     video.onloadedmetadata = resizeMedia.bind(video);
