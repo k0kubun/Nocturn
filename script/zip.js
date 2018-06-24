c8eed983c61bfcb5cfd4bb390da58b50a8f41668
Takashi Kokubun <takashikkbn@gmail.com>
2018-06-24 11:13:14 +0900

    Optionally zip packages

--- script/zip.js
+++ script/zip.js
@@ -3,8 +3,17 @@
 require('shelljs/global');
 const version = require('./version').version;
 
-exec(`cd packages/v${version} && zip -r --symlinks Nocturn-darwin-x64.zip Nocturn-darwin-x64`)
-exec(`cd packages/v${version} && zip -r --symlinks Nocturn-linux-x64.zip Nocturn-linux-x64`)
-exec(`cd packages/v${version} && zip -r --symlinks Nocturn-win32-x64.zip Nocturn-win32-x64`)
-exec(`cd packages/v${version} && zip -r --symlinks Nocturn-linux-ia32.zip Nocturn-linux-ia32`)
-exec(`cd packages/v${version} && zip -r --symlinks Nocturn-win32-ia32.zip Nocturn-win32-ia32`)
+[
+  'Nocturn-darwin-x64',
+  'Nocturn-linux-x64',
+  'Nocturn-win32-x64',
+  'Nocturn-linux-ia32',
+  'Nocturn-win32-ia32',
+].forEach((name) => {
+  exec(`
+    cd "packages/v${version}"
+    if [ -d "${name}" ]; then
+      zip -r --symlinks "${name}.zip" "${name}"
+    fi
+  `)
+});
